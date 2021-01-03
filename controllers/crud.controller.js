1fe7cc6b4f2d21824851a32871f628ffb5685a20
manassorn <manassorn@gmail.com>
2021-01-04 06:00:07 +0700

    a

--- controllers/crud.controller.js
+++ controllers/crud.controller.js
@@ -1,4 +1,6 @@
 var firestore = require('../services/firestore.service')
+var sendgrid = require('../services/sendgrid.service')
+
 
 module.exports.create = (collection, payload) => {
     firestore.create(collection, payload)
@@ -10,5 +12,6 @@ module.exports.update = (collection, id, payload) => {
 
 module.exports.read = async (collection) => {
   const data = await firestore.read(collection)
+  await sendgrid.send('manassorn@gmail.com','test','test')
   return data;
 }
---
+++ services/sendgrid.services.js
@@ -0,0 +1,24 @@
+const sgMail = require('@sendgrid/mail');
+
+const conf = require('../conf/sendgrid.json')
+
+sgMail.setApiKey(conf.apiKey);
+
+module.exports.send = async (to,subject,text) => {
+  const msg = {
+    to: to,
+    from: 'test@example.com', // Use the email address or domain you verified above
+    subject: subject,
+    text: text,
+  };
+  try {
+    await sgMail.send(msg);
+  } catch (error) {
+    console.error(error);
+  
+    if (error.response) {
+      console.error(error.response.body)
+    }
+  }
+}
+
