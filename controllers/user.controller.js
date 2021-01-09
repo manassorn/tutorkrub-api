6ae57b330688dcf7b3b9de50c5d5704bf7bca859
manassorn <manassorn@gmail.com>
2021-01-09 17:10:01 +0700

    a

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,7 +1,7 @@
 var firestore = require('../services/firestore.service')
 
 module.exports.login = async (email, password) => {
-  const snapshot = await firestore.firestore.collection('user').where('email', '==', email).get()
+  const snapshot = await firestore.firestore.collection('user').where('email', '==', email).where('password', '==', password).get()
   console.log(snapshot.docs.length)
   return snapshot.docs.map(doc => doc.data()).length != 0;
 }
