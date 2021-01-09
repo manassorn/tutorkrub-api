f1350e25410cb79d9757584fdd6f235032fb8730
manassorn <manassorn@gmail.com>
2021-01-09 11:46:36 +0700

    a

--- routes/user.js
+++ routes/user.js
@@ -1,18 +1,23 @@
 var express = require('express');
 var router = express.Router();
 
+var api = require('./api')
 var verificationController = require('../controllers/verification.controller')
+var crudController = require('../controllers/crud.controller')
 
-router.post('/', function(req, res, next) {
-  if(verificationController.verifyForEmail(req.body.email, req.body.code)) {
-    const payload = {
-      email: req.body.email,
-      name: req.body.name,
-      password: req.body.password
-    }
-    crud.create('user', payload)
+router.post('/', async(req, res, next) => {
+  var passed = await verificationController.verifyForEmail(req.body.email, req.body.code)
+  if(passed === false) {
+    api.responseError('code is incorrect')
+    return false
   }
-  res.send('respond with a resource');
+  const payload = {
+    email: req.body.email,
+    name: req.body.name,
+    password: req.body.password
+  }
+  await crudController.create('user', payload)
+  api.responseOk(res)
 });
 
 module.exports = router;
