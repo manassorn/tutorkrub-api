a8cdc01f8e7f210ce0fe1f8ceecdafb2adeed952
mvanichdilok <manassorn.vanichdilokkul@agoda.com>
2021-02-02 13:58:37 +0700

    a

--- routes/api.js
+++ routes/api.js
@@ -6,8 +6,8 @@ module.exports.responseOk = (res, data) => {
         data: data
     })
 }
-module.exports.responseError = (res, message) => {
-    res.json({
+module.exports.responseError = (res, code = 200, message) => {
+    res.status(code).json({
         error: {
             message: message
         }
