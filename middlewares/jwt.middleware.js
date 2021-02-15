e2b2cce9558ee20c28d256a34b7d04af4f83afd1
mvanichdilok <manassorn.vanichdilokkul@agoda.com>
2021-02-15 16:58:50 +0700

    a

---
+++ middlewares/jwt.middleware.js
@@ -0,0 +1,15 @@
+module.exports.authenticateToken = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+    if (authHeader) {
+        const token = authHeader.split(' ')[1];
+        jwt.verify(token, process.env.TOKEN_SECRET, (err, user) => {
+            if (err) {
+                return res.sendStatus(403);
+            }
+            req.user = user;
+            next();
+        });
+    } else {
+        res.sendStatus(401);
+    }
+};
--- routes/authen.js
+++ routes/authen.js
@@ -42,7 +42,7 @@ module.exports.authenticateJWT = (req, res, next) => {
   const authHeader = req.headers.authorization;
   if (authHeader) {
     const token = authHeader.split(' ')[1];
-    jwt.verify(token, process.env, (err, user) => {
+    jwt.verify(token, process.env.TOKEN_SECRET, (err, user) => {
       if (err) {
         return res.sendStatus(403);
       }
