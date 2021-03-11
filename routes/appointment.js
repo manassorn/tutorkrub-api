3aee462375e6d0bc5938ad4ca74389c29e732643
manassorn <manassorn@gmail.com>
2021-03-11 12:58:56 +0700

    a

--- routes/appointment.js
+++ routes/appointment.js
@@ -3,20 +3,44 @@ var router = express.Router();
 
 var api = require('./api')
 var crudController = require('../controllers/crud.controller')
-var userController = require('../controllers/user.controller')
+var firestoreService = require('../services/firestore.service')
 
 
 router.post('/course/:id', async (req, res, next) => {
-  const userId = req.user.userId
   const courseId = req.params.courseId
+  const course = await crudController.readById(courseId)
+  const tutorId = course.tutorId
+  const studentId = req.user.userId
   const payload = {
-    courseId, startTime, length, 
-    status: 'to_be_paid', //to_be_accepted, to_be_started, complete
-    teacherId: userId,
-    studentId: userId
+    courseId, startTime, length, tutorId, studentId, 
+    status: 'to_be_paid' //to_be_accepted, to_be_started, complete
   }
   const appointment = await crudController.create('appointment', payload)
   
   api.responseOk(res, appointment)
   
 });
+
+router.get('tutor/status/:status', (req, res, next) => {
+  const
+  const snapshot = await firestore.firestore.collection('appointment').where('tutorId', '==', req.user.userId).where('status', '==', req.params.status).get()
+  
+  const appointments = snapshot.docs.map(doc => {
+    const id = doc.id
+    const data = doc.data()
+    return { id, ...data }
+  });
+  return appointments;
+})
+
+router.get('student/status/:status', (req, res, next) => {
+  const
+  const snapshot = await firestore.firestore.collection('appointment').where('studentId', '==', req.user.userId).where('status', '==', req.params.status).get()
+
+  const appointments = snapshot.docs.map(doc => {
+    const id = doc.id
+    const data = doc.data()
+    return { id, ...data }
+  });
+  return appointments;
+})
---
+++ routes/course.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+
+var api = require('./api')
+var crudController = require('../controllers/crud.controller')
+
+
+router.post('/', async (req, res, next) => {
+  const title = req.body.title
+  const description = req.body.description
+  const price = req.body.price
+  const category = req.body.category
+  const tutorId = req.user.userId
+  
+  const payload = {title, description, price, category, tutorId}
+  
+  const course = await crudController.create('course', payload)
+  
+  api.responseOk(res, course)
+  
+});
