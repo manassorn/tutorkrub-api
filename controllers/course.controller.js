9948df00abc1618f94daffbce28e6cfaab2c358d
manassorn <manassorn@gmail.com>
2021-03-12 14:32:10 +0700

    yy

--- controllers/course.controller.js
+++ controllers/course.controller.js
@@ -6,7 +6,7 @@ var crudController = require('./crud.controller')
 module.exports.list = async (collection, payload) => {
   var courses = await crudController.read('course')
   var tutorIds = courses.map(course => course.tutorId).filter(id => id !== undefined)
-  var snapshot = await firestoreService.firestore.collection('user').where('_id', 'in', tutorIds).get()
+  var snapshot = await firestoreService.firestore.collection('user').where(firestoreService.firestore.FieldPath.documentId(), 'in', tutorIds).get()
   var users = firestoreService.toList(snapshot)
   var userAvatars = {}
   users.map(user => {
