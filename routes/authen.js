7cdb0c8b2ec44b59679360a0ebe6e7286f4df04a
manassorn <manassorn@gmail.com>
2021-03-12 14:04:55 +0700

    ww

--- routes/authen.js
+++ routes/authen.js
@@ -11,7 +11,7 @@ router.post('/login', async (req, res, next) => {
   const user = await userController.getUserByEmailPassword(req.body.email, req.body.password)
   if (user) {
     // Generate an access token
-    const accessToken = jwt.sign({ userId: user.id }, process.env.TOKEN_SECRET);
+    const accessToken = jwt.sign({ userId: user.id }, process.env.TOKEN_SECRET, {expiresIn: 60});
     res.cookie('accesstoken', accessToken, { expires: new Date(Date.now() + 900000), httpOnly: true, secure: true })
     api.responseOk(res, {accessToken})
   } else {
@@ -20,7 +20,7 @@ router.post('/login', async (req, res, next) => {
 });
 
 router.get('/1clicklogin', async (req, res, next) => {
-  const accessToken = jwt.sign({ userId: 'hNqOKzYwhJjZTIDLUkf5' }, process.env.TOKEN_SECRET);
+  const accessToken = jwt.sign({ userId: 'hNqOKzYwhJjZTIDLUkf5' }, process.env.TOKEN_SECRET, {expiresIn: 60});
   res.cookie('accesstoken', accessToken, { expires: new Date(Date.now() + 900000), httpOnly: true, secure: true })
   api.responseOk(res, {accessToken})
 });
