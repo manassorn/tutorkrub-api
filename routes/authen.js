ca0c2e069a2dca3d1ed60ecaf4bd44a0b5327664
manassorn <manassorn@gmail.com>
2021-05-11 11:56:53 +0700

    dd

--- routes/authen.js
+++ routes/authen.js
@@ -56,7 +56,8 @@ router.post('/login', async (req, res, next) => {
 
 router.get('/devlogin2/:userId', async (req, res, next) => {
   //hNqOKzYwhJjZTIDLUkf5
-  const userId = req.params.userId.condole.log('qqqq')
+  const userId = req.params.userId
+  console.log('qqqq')
   generateJwtToken(res, userId)
   const user = crudController.readById('user', userId)
   console.log('agags')
