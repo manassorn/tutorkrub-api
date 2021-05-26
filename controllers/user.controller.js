00dec669523007e4ff76dfb51b8d07320a24c429
manassorn <manassorn@gmail.com>
2021-05-26 10:53:10 +0700

    bb

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -2,7 +2,7 @@ var firestoreService = require('../services/firestore.service')
 var admin = require('firebase-admin')
 
 module.exports.getUserByEmailPassword = async (email, password) => {
-  const snapshot = await firestoreService.firestore.collection('user').where('email', '==', email).where('password', '==', password).get()
+  const snapshot = await firestoreService.firestore.collection('Users').where('email', '==', email).where('password', '==', password).get()
 
   const users = snapshot.docs.map(doc => {
     const id = doc.id
@@ -14,7 +14,7 @@ module.exports.getUserByEmailPassword = async (email, password) => {
 
 module.exports.getByIdList = async(idList) => {
   if(idList.length == 0) return []
-  const snapshot = await firestoreService.firestore.collection('user').where(admin.firestore.FieldPath.documentId(), 'in', idList).get()
+  const snapshot = await firestoreService.firestore.collection('Users').where(admin.firestore.FieldPath.documentId(), 'in', idList).get()
   const users = firestoreService.toList(snapshot)
   return users
 }
