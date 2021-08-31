613b542d9c6f9e6c57f9c7af9c7c26ef881e4320
manassorn <manassorn@gmail.com>
2021-08-31 20:53:30 +0700

    g

--- controllers/authen.controller.js
+++ controllers/authen.controller.js
@@ -2,7 +2,7 @@ var loginAccountDao = require('../dao/LoginAccountDao')
 
 
 module.exports.login = async (email, pwd) => {
-  const loginAccount = loginAccountDao.getByEmail(email)
+  const loginAccount = await loginAccountDao.getByEmail(email)
   console.log(loginAccount)
   const match = await loginAccount.comparePassword(pwd)
   if(!match) return
