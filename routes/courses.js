0650ca41043042bde861357a09b6c835f85669ea
manassorn <manassorn@gmail.com>
2021-09-03 21:45:54 +0700

    djf

--- routes/courses.js
+++ routes/courses.js
@@ -9,7 +9,9 @@ var courseController = require('../controllers/course.controller')
 
 
 router.post('/', async (req, res, next) => {
-  await courseController.create(req.body)
+  const data = req.body
+  data.tutor = req.user.id
+  await courseController.create(data)
   const title = req.body.title
   const description = req.body.description
   const schoolLevel = req.body.schoolLevel
