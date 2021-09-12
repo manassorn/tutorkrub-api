9038967b7c37bbbe526aa7e3b89a122a020cf3f3
manassorn <manassorn@gmail.com>
2021-09-12 10:44:47 +0700

    fff

--- routes/register.js
+++ routes/register.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 var api = require('./api')
 const loginAccountDao = require('../dao/LoginAccountDao')
-const userDao = require('../dao/UserDao')
+const userDao = require('../dao/users.dao')
 
 router.post('/', async (req, res, next) => {
   const user = await userDao.create(req.body.user)
