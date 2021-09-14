143d20e52e600fc5e6d59ac9181ea95ddd43c1a6
manassorn <manassorn@gmail.com>
2021-09-14 21:34:16 +0700

    gf

--- dao/courses.dao.js
+++ dao/courses.dao.js
@@ -7,13 +7,17 @@ class CoursesDao extends BaseDao {
   }
   
   async getListByOwner(tutorId) {
-    const courses = await Course.find({'tutor': tutorId}).populate('tutor').exec()
+    const courses = await Course.find({'tutor': tutorId}).exec()
     return courses
   }
   
   async getAll() {
     const courses = await Course.find().populate('tutor').exec()
     return courses
+  }
+  
+  async get(courseId) {
+    return await Course.findById(courseId).populate('tutor').exec()
   }
 }
 
