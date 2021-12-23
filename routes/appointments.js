ab335259dc479ac1ecdc87fc67ea5c7ddc45e2ce
manassorn <manassorn@gmail.com>
2021-12-23 19:29:52 +0700

    fhf

--- routes/appointments.js
+++ routes/appointments.js
@@ -47,7 +47,7 @@ router.post('/', async (req, res, next) => {
   const course = await coursesController.get(req.body.courseId)
 appointment.tutor = course.tutor  
   
-  const appointment2 = await appointmentsController.create('Appointments', payload)
+  const appointment2 = await appointmentsController.create('Appointments', appointment)
   
   api.responseOk(res, appointment2)
   
