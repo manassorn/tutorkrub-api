672da201900298012076c40195390026fe1e4d55
mvanichdilok <manassorn.vanichdilokkul@agoda.com>
2022-04-10 12:56:47 +0700

    a

--- routes/favorite.js
+++ routes/favorite.js
@@ -5,12 +5,12 @@ var api = require('./api')
 const favoriteDao = require('../dao/favorite.dao')
 
 
-router.put('/:tutorId', async (req, res, next) => {
+router.put('/tutor/:tutorId', async (req, res, next) => {
   await tutorsDao.add(req.user.id, req.query.tutorId)
   api.ok(res)
 });
 
-router.delete('/:tutorId', async (req, res, next) => {
+router.delete('/tutor/:tutorId', async (req, res, next) => {
   await tutorsDao.remove(req.user.id, req.query.tutorId)
   api.ok(res)
 });
