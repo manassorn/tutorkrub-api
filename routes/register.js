a8163e42e3fb0e30f46398b7c7e9f730ea2ed303
mvanichdilok <manassorn.vanichdilokkul@agoda.com>
2022-05-24 20:08:14 +0700

    a

--- routes/register.js
+++ routes/register.js
@@ -6,11 +6,16 @@ const loginAccountDao = require('../dao/LoginAccountDao')
 const userDao = require('../dao/users.dao')
 
 router.post('/', async (req, res, next) => {
-  const user = await userDao.create(req.body.user)
-  const loginAccount = req.body.loginAccount
-  loginAccount.user = user.id
+  if (req.body.user) {
+    const user = await userDao.create(req.body.user)
+    const loginAccount = req.body.loginAccount
+    loginAccount.user = user.id
 
-  await loginAccountDao.create(loginAccount)
+    await loginAccountDao.create(loginAccount)
+  } else {
+    const loginAccount = req.body.loginAccount
+    await loginAccountDao.create(loginAccount)
+  }
 
   api.ok(res)
 
