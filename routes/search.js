73c77a9c9d63fc9b6dce29bb0d2ce051be6ae049
mvanichdilok <manassorn.vanichdilokkul@agoda.com>
2022-06-17 11:14:23 +0700

    a

--- routes/search.js
+++ routes/search.js
@@ -8,6 +8,12 @@ const tutorDao = require('../dao/tutor.dao')
 router.get('/', async (req, res, next) => {
   const subject = req.params.subject
   const level = req.params.level
+  const tutorId = req.params.tutorid
+  if (tutorId) {
+    const tutor = await tutorDao.get(tutorId)
+    api.ok(res, mapTutor(tutor))
+  }
+
   const tutors = await tutorDao.search(subject, level)
   const searches = tutors.map((t) => {
     return {
@@ -22,5 +28,16 @@ router.get('/', async (req, res, next) => {
   api.ok(res, searches)
 });
 
+function mapTutor(t) {
+  //this should only return public data
+  return {
+    id: t._id,
+      tutorSubjects: t.tutorSubjects,
+    tutorLevels: t.tutorLevels,
+    tutorPrice: t.tutorPrice,
+    krubId: t.user.krubId,
+    avatarUrl: t.user.avatarUrl
+  }
+}
 
 module.exports = router;
