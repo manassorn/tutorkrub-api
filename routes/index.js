21bdf43375a93bb52ddf5c9ca13011ca0d7ab117
mvanichdilok <manassorn.vanichdilokkul@agoda.com>
2022-07-04 20:40:59 +0700

    a

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.sendFile('public/index.html', { title: 'TutorKrub' });
+  res.sendFile(path.join(__dirname,'public/index.html'), { title: 'TutorKrub' });
 });
 
 module.exports = router;
