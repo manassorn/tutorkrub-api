b3094437622cc03878cec6102df3ec4114b80328
mvanichdilok <manassorn.vanichdilokkul@agoda.com>
2022-08-31 22:10:32 +0700

    a

--- notification/notification-center.js
+++ notification/notification-center.js
@@ -3,9 +3,9 @@ var templateEngine = require('./email-template/template-engine')
 var loginAccountDao = require('../dao/loginAccount.dao')
 
 async function sendEmail(userId, templateName, data) {
-  // const loginAccount = await loginAccountDao.getByUser(userId)
-  // const emailAddress = loginAccount.email
-  const emailAddress = 'manassorn@gmail.com'
+  const loginAccount = await loginAccountDao.getByUser(userId)
+  const emailAddress = loginAccount.email
+  // const emailAddress = 'manassorn@gmail.com'
   console.log('emailAddress',emailAddress,userId)
   const subject = templateEngine.hbsTemplate(templateName + '-subject')
   const body = templateEngine.hbsTemplate(templateName)
