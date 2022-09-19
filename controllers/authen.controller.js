72182afb339c7347b855db645889c5cf442f3f6d
mvanichdilok <manassorn.vanichdilokkul@agoda.com>
2022-09-19 16:43:44 +0700

    a

--- controllers/authen.controller.js
+++ controllers/authen.controller.js
@@ -3,7 +3,8 @@ var loginAccountDao = require('../dao/loginAccount.dao')
 
 module.exports.login = async (email, pwd) => {
   const loginAccount = await loginAccountDao.getByEmail(email)
-  console.log(loginAccount)
+  if(loginAccount == undefined) return
+
   const match = await loginAccount.comparePassword(pwd)
   if(!match) return
   else return loginAccount.user
